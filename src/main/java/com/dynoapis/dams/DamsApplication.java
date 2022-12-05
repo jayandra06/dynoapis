5e3853fc81e4b1c6a88e76b228450b0d010a2d75
dynoapis <110982207+dynoapis@users.noreply.github.com>
2022-12-05 22:40:13 +0530

    Update DamsApplication.java

--- src/main/java/com/dynoapis/dams/DamsApplication.java
+++ src/main/java/com/dynoapis/dams/DamsApplication.java
@@ -1,20 +1,10 @@
 package com.dynoapis.dams;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DamsApplication {
-
-	@PostConstruct
-	public void init(){
-		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
-	  }
-	
+public class DamsApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DamsApplication.class, args);
