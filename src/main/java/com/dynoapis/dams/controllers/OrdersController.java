7daa432948a539e3634b4119d37d9b7272af602f
dynoapis <110982207+dynoapis@users.noreply.github.com>
2022-12-05 22:32:39 +0530

    Update OrdersController.java

--- src/main/java/com/dynoapis/dams/controllers/OrdersController.java
+++ src/main/java/com/dynoapis/dams/controllers/OrdersController.java
@@ -13,10 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dynoapis.dams.entity.ErrorEntity;
+import com.dynoapis.dams.exceptions.DBException;
+import com.dynoapis.dams.model.ErrorRequest;
 import com.dynoapis.dams.model.OrderRequest;
+import com.dynoapis.dams.model.OrderStatus;
+import com.dynoapis.dams.repository.ErrorRepository;
 import com.dynoapis.dams.service.OrderService;
 
 @RestController
@@ -25,7 +29,10 @@ public class OrdersController {
 
     @Autowired
     private OrderService orderService;
-    
+
+    @Autowired
+    private ErrorRepository errorRepository;
+        
     @GetMapping("/")
     public Map<String, Object> index() {
         Map<String, Object> response = new HashMap<>();
@@ -34,37 +41,52 @@ public class OrdersController {
         return response;
     }
 
-    @PostMapping("/neworder")
-    public Map<String, Object> insertNewOrder(@RequestBody OrderRequest order) {
+    @PostMapping("/api/v1/orders")
+    public List<Map<String, Object>> insertNewOrders(@RequestBody OrderRequest order) {
         return orderService.saveOrder(order);
     }
 
-    @GetMapping("/{restaurantId}/order")
-    public List<Object> getOrder(@RequestParam String token, @PathVariable String restaurantId) {
+    @GetMapping("/api/v1/{restaurantId}/orders")
+    public List<Object> getOrders(@PathVariable String restaurantId) {
         Calendar startToday = Calendar.getInstance();
         TimeZone tz = TimeZone.getTimeZone("IST");
         startToday.setTimeZone(tz);
-        startToday.add(Calendar.HOUR_OF_DAY, -24);
-        startToday.set(Calendar.MINUTE, 0);
-        startToday.set(Calendar.SECOND, 0);
-        startToday.set(Calendar.MILLISECOND, 0);
-        Timestamp startDate = new Timestamp(startToday.getTimeInMillis());
-        return orderService.getUnProcessedOrders(String.format("%s_%s", token, restaurantId ), startDate, new Timestamp(System.currentTimeMillis()));
-    }
-
-    @GetMapping("/{restaurantId}/orders")
-    public List<Object> getOrders(@RequestParam String token, @PathVariable String restaurantId) {
-        Calendar startToday = Calendar.getInstance();
-        TimeZone tz = TimeZone.getTimeZone("IST");
-        startToday.setTimeZone(tz);
-        startToday.add(Calendar.HOUR_OF_DAY, -24);
+        startToday.add(Calendar.HOUR_OF_DAY, -25);
         startToday.set(Calendar.MINUTE, 0);
         startToday.set(Calendar.SECOND, 0);
         startToday.set(Calendar.MILLISECOND, 0);
         Timestamp startDate = new Timestamp(startToday.getTimeInMillis());
         Timestamp endDate = new Timestamp(System.currentTimeMillis());
-        return orderService.getOrders(String.format("%s_%s", token, restaurantId ), startDate, endDate);
+        return orderService.getOrders(restaurantId, startDate, endDate);
     }
 
+    @GetMapping("/api/v1/{restaurantId}/orders/status")
+    public List<Map<String, Object>> getActionsToBePerformed(@PathVariable String restaurantId) {
+        return orderService.getOrdersByStatus(restaurantId);
+    }
+
+
+    @PostMapping("/api/v1/orders/{orderId}/status")
+    public Map<String, Object> getActionsPerformed(@PathVariable String orderId, @RequestBody OrderStatus order) {
+        return orderService.saveOrderStatus(orderId, order.getStatusCode(), order.getStatusResponse());
+    }
+
+
+    @PostMapping("/api/v1/errors")
+    public Map<String, Object> sendErrors(@RequestBody ErrorRequest error) {
+        try {
+            ErrorEntity errorEntity = new ErrorEntity();
+            errorEntity.setAccessToken(error.getAccessToken());
+            errorEntity.setErrorMessage(error.getErrorMessage());
+            errorEntity.setResId(error.getResId());
+            errorRepository.save(errorEntity);
+        } catch (Exception e) {
+            throw new DBException(e.getMessage());
+        }
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", true);
+        response.put("message", "Inserted Successfully!");
+        return response;
+    }
 
 }
