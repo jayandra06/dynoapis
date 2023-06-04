d0af1480a3797ffdac9a023a8faec5456256f5c5
dynoapis <110982207+dynoapis@users.noreply.github.com>
2023-06-04 11:39:03 +0530

    Update OrderHistoryRepository.java

--- src/main/java/com/dynoapis/dams/repository/OrderHistoryRepository.java
+++ src/main/java/com/dynoapis/dams/repository/OrderHistoryRepository.java
@@ -1,15 +1,11 @@
 package com.dynoapis.dams.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dynoapis.dams.entity.OrderHistoryEntity;
 
 public interface OrderHistoryRepository extends JpaRepository<OrderHistoryEntity, Long> {
     
-    List<OrderHistoryEntity> findByRestaurantId(String resId);
-
-    OrderHistoryEntity findByRestaurantIdAndAggregator(String resId, String aggregator);
+    OrderHistoryEntity findByRestaurantId(String resId);
 
 }
