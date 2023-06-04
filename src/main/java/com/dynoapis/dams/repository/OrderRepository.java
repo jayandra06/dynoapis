5ecce932dd55d2641d065d889556bb9da34864a0
dynoapis <110982207+dynoapis@users.noreply.github.com>
2023-06-04 10:30:50 +0530

    Update OrderRepository.java

--- src/main/java/com/dynoapis/dams/repository/OrderRepository.java
+++ src/main/java/com/dynoapis/dams/repository/OrderRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
     List<OrderEntity> findByRestaurantIdAndCreatedAtBetween(String resId, Timestamp startDate, Timestamp endDate);
-    
+    List<OrderEntity> findByCreatedAtBetween(Timestamp startDate, Timestamp endDate);
+
     OrderEntity findByOrderId(String orderId);
 
     List<OrderEntity> findByRestaurantId(String resId);
