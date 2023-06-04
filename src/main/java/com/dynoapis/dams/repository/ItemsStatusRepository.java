5a8fe81671b9e16088f324ea11bb12f641eca3cd
dynoapis <110982207+dynoapis@users.noreply.github.com>
2023-06-04 11:56:25 +0530

    Update ItemsStatusRepository.java

--- src/main/java/com/dynoapis/dams/repository/ItemsStatusRepository.java
+++ src/main/java/com/dynoapis/dams/repository/ItemsStatusRepository.java
@@ -1,15 +1,11 @@
 package com.dynoapis.dams.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dynoapis.dams.entity.ItemsStatusEntity;
 
 public interface ItemsStatusRepository extends JpaRepository<ItemsStatusEntity, Long> {
-    
-    List<ItemsStatusEntity> findByRestaurantId(String resId);
 
-    ItemsStatusEntity findByRestaurantIdAndAggregator(String resId, String aggregator);
+    ItemsStatusEntity findByRestaurantId(String resId);
 
 }
